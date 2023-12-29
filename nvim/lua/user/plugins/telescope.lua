c2cd9feb4389d203582f4a0693d7b2d4df9e7cae
Anh Mac <git@macanh.com>
2023-12-29 17:31:32 +0700

    feat: nvm

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -36,7 +36,7 @@ return {
             ['<C-Up>'] = actions.cycle_history_prev,
           },
         },
-        file_ignore_patterns = { '.git/' },
+        file_ignore_patterns = { '.git/', 'node_modules' },
       },
       extensions = {
         live_grep_args = {
