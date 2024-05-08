1e382ff3a1a120e2ac706f1d63957ea617d71624
Anh Mac <git@macanh.com>
2024-05-08 17:48:33 +0700

    outline

--- nvim/lua/user/plugins/outline.lua
+++ nvim/lua/user/plugins/outline.lua
@@ -5,8 +5,6 @@ return {
     vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
       { desc = "Toggle Outline" })
 
-    require("outline").setup {
-      -- Your setup opts here (leave empty to use defaults)
-    }
+    require("outline").setup {}
   end,
 }
